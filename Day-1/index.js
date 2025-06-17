2f4f45214bd62a30941ebc71d7530849e9e5d47f
ThrishikK <kosuruthrishik@gmail.com>
2025-06-17 11:20:45 +0530

    Started Js DS from GPT 180 days pattern

---
+++ Day-1/index.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = "hello";
+let c = true;
+let d;
+let e = null;
+let f = Symbol("id");
+let g = 9007199254740991n;
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof d);
+console.log(typeof e);
+console.log(typeof f);
+console.log(typeof g);
